c4e383a118331bfbfbd0c00f7086553a0a70f906
Ívar Árnason <ivar1309@gmail.com>
2025-08-15 12:39:00 +0000

    fix: missing POST in cors

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -61,7 +61,7 @@ func withPublicCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set dynamic CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		if r.Method == http.MethodOptions {
